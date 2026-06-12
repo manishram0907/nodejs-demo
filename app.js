2d8a980e48321d3b8e20780f077ddf77b977750c
manishram0907 <90261621+manishram0907@users.noreply.github.com>
2026-06-12 14:36:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello!! This is th efirst changed and test automation');
+    res.send('Hello!! This is the first changed and testing automation');
 });
 
 app.listen(3000, () => {
